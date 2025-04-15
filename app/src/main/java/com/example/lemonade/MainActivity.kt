ea7361ac0fed7da77eabdd276e3b980de0eaf404
Eder Hernández <dariotecsoft90@gmail.com>
2025-04-15 12:11:44 -0500

    modifity code for aggregation topbar and title in activity

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -6,16 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -23,9 +30,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,15 +47,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             LemonadeTheme {
             }
-            LemonadeApp(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .wrapContentSize(Alignment.Center)
-            )
+            LemonadeApp()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LemonadeApp(
     modifier: Modifier = Modifier
@@ -54,44 +60,68 @@ fun LemonadeApp(
     var currentStep by remember { mutableIntStateOf(1) }
     var squeezeCount by remember { mutableIntStateOf(1) }
 
-    when(currentStep) {
-        1 -> LemonadeWithButtonAndImage(
-            textLabelResourceId = R.string.lemon_tree_text,
-            drawableResourceId = R.drawable.lemon_tree,
-            contentDescriptionResourceId = R.string.lemon_tree_content_description,
-            onImageClick = {
-                currentStep = 2
-                squeezeCount = (2..4).random()
-            },
-            modifier
-        )
-        2 -> LemonadeWithButtonAndImage(
-            textLabelResourceId = R.string.lemon_text,
-            drawableResourceId = R.drawable.lemon_squeeze,
-            contentDescriptionResourceId = R.string.lemon_content_description,
-            onImageClick = {
-                squeezeCount--
-                if(squeezeCount == 0) {
-                    currentStep = 3
-                }
+    Scaffold (
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = colorResource(R.color.top_bar_gold)
+                )
+            )
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            when(currentStep) {
+                1 -> LemonadeWithButtonAndImage(
+                    textLabelResourceId = R.string.lemon_tree_text,
+                    drawableResourceId = R.drawable.lemon_tree,
+                    contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                    onImageClick = {
+                        currentStep = 2
+                        squeezeCount = (2..4).random()
+                    },
+                    modifier
+                )
+                2 -> LemonadeWithButtonAndImage(
+                    textLabelResourceId = R.string.lemon_text,
+                    drawableResourceId = R.drawable.lemon_squeeze,
+                    contentDescriptionResourceId = R.string.lemon_content_description,
+                    onImageClick = {
+                        squeezeCount--
+                        if(squeezeCount == 0) {
+                            currentStep = 3
+                        }
+                    }
+                )
+                3 -> LemonadeWithButtonAndImage(
+                    textLabelResourceId = R.string.glass_of_lemonade_text,
+                    drawableResourceId = R.drawable.lemon_drink,
+                    contentDescriptionResourceId = R.string.glass_of_lemonade_content_description,
+                    onImageClick = {
+                        currentStep = 4
+                    }
+                )
+                4 -> LemonadeWithButtonAndImage(
+                    textLabelResourceId = R.string.empty_glass_text,
+                    drawableResourceId = R.drawable.lemon_restart,
+                    contentDescriptionResourceId = R.string.empty_glass_content_description,
+                    onImageClick = {
+                        currentStep = 1
+                    }
+                )
             }
-        )
-        3 -> LemonadeWithButtonAndImage(
-            textLabelResourceId = R.string.glass_of_lemonade_text,
-            drawableResourceId = R.drawable.lemon_drink,
-            contentDescriptionResourceId = R.string.glass_of_lemonade_content_description,
-            onImageClick = {
-                currentStep = 4
-            }
-        )
-        4 -> LemonadeWithButtonAndImage(
-            textLabelResourceId = R.string.empty_glass_text,
-            drawableResourceId = R.drawable.lemon_restart,
-            contentDescriptionResourceId = R.string.empty_glass_content_description,
-            onImageClick = {
-                currentStep = 1
-            }
-        )
+        }
     }
 }
 
@@ -112,7 +142,7 @@ fun LemonadeWithButtonAndImage(
             onClick = onImageClick,
             shape = RoundedCornerShape(40.dp),
             colors = ButtonDefaults.buttonColors(colorResource(R.color.green_mint)),
-            border = BorderStroke(0.2.dp, colorResource(R.color.teal_200))
+            border = BorderStroke(0.8.dp, colorResource(R.color.border_green))
         ) {
             Image(
                 painter = painterResource(drawableResourceId),
@@ -135,10 +165,6 @@ fun LemonadeWithButtonAndImage(
 @Composable
 fun GreetingPreview() {
     LemonadeTheme {
-        LemonadeApp(
-            modifier = Modifier
-                .fillMaxSize()
-                .wrapContentSize(Alignment.Center)
-        )
+        LemonadeApp()
     }
 }

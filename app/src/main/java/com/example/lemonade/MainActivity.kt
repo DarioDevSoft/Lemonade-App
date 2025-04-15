5d43a1e3dc30776125dc78474555e10acc2c6251
Eder Hernández <dariotecsoft90@gmail.com>
2025-04-15 11:10:20 -0500

    modifity code en activity

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,9 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,15 +37,87 @@ class MainActivity : ComponentActivity() {
         setContent {
             LemonadeTheme {
             }
+            LemonadeApp(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center)
+            )
         }
     }
 }
 
 @Composable
-fun LemonadeWithButtonAndImage(
+fun LemonadeApp(
     modifier: Modifier = Modifier
 ) {
+    var currentStep by remember { mutableIntStateOf(1) }
 
+    when(currentStep) {
+        1 -> LemonadeWithButtonAndImage(
+            textLabelResourceId = R.string.lemon_tree_text,
+            drawableResourceId = R.drawable.lemon_tree,
+            contentDescriptionResourceId = R.string.lemon_tree_content_description,
+            onImageClick = {
+                currentStep = 2
+            },
+            modifier
+        )
+        2 -> LemonadeWithButtonAndImage(
+            textLabelResourceId = R.string.lemon_text,
+            drawableResourceId = R.drawable.lemon_squeeze,
+            contentDescriptionResourceId = R.string.lemon_content_description,
+            onImageClick = {
+                currentStep = 3
+            }
+        )
+        3 -> LemonadeWithButtonAndImage(
+            textLabelResourceId = R.string.glass_of_lemonade_text,
+            drawableResourceId = R.drawable.lemon_drink,
+            contentDescriptionResourceId = R.string.glass_of_lemonade_content_description,
+            onImageClick = {
+                currentStep = 4
+            }
+        )
+        4 -> LemonadeWithButtonAndImage(
+            textLabelResourceId = R.string.empty_glass_text,
+            drawableResourceId = R.drawable.lemon_restart,
+            contentDescriptionResourceId = R.string.empty_glass_content_description,
+            onImageClick = {
+                currentStep = 1
+            }
+        )
+    }
+}
+
+@Composable
+fun LemonadeWithButtonAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = onImageClick,
+            shape = RoundedCornerShape(40.dp),
+            colors = ButtonDefaults.buttonColors(colorResource(R.color.green_mint))
+        ) {
+            Image(
+                painter = painterResource(drawableResourceId),
+                contentDescription = stringResource(contentDescriptionResourceId)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(textLabelResourceId),
+            fontSize = 18.sp
+        )
+    }
 }
 
 @Preview(
@@ -35,5 +128,10 @@ fun LemonadeWithButtonAndImage(
 @Composable
 fun GreetingPreview() {
     LemonadeTheme {
+        LemonadeApp(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        )
     }
 }
